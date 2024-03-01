7ce36647d175d1547414463664bc5fe58ea32d02
abhishekabhiRaj <bcoder48@gmail.com>
2024-03-01 10:38:53 +0530

    Visitor Controller

--- backend/src/models/VisitorModel.js
+++ backend/src/models/VisitorModel.js
@@ -7,8 +7,12 @@ const VisitorSchema = new mongoose.Schema({
     visitor_mobile:{type:Number, required:true},
     visitor_email:{type:String, required:true},
     visitor_address:{type:String, required:true},
+    visitor_login:{type:String},
+    visitor_logout:{type:String},
     visitor_image:{type:String},
     visit_date:{type:Date},
+    visit_status:{type:String, required:true},
+    visit_completed:{type:String, required:true},
     // Person To Meet Fields
     ptm_name:{type:String, required:true},
     ptm_mobile:{type:Number, required:true},
