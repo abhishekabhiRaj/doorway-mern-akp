f89f79288912432d7918656c4edba3955132e0f0
abhishekabhiRaj <bcoder48@gmail.com>
2024-03-13 20:22:23 +0530

    doorway approval api

--- backend/src/controllers/VisitorController.js
+++ backend/src/controllers/VisitorController.js
@@ -2,7 +2,7 @@ import { VisitorModel } from "../models/VisitorModel.js";
 
 var createVisitorController = async (req, res) => {
     try {
-        const { 
+        const {
             visitor_name,
             visitor_mobile,
             visitor_email,
@@ -13,7 +13,7 @@ var createVisitorController = async (req, res) => {
             ptm_name,
             ptm_mobile,
             ptm_email,
-            ptm_address, 
+            ptm_address,
         } = req.body;
         const newVisitor = new VisitorModel({
             visitor_name,
@@ -22,11 +22,11 @@ var createVisitorController = async (req, res) => {
             visitor_address,
             visitor_image,
             visitor_purpose,
-            visitor_login:"",
-            visitor_logout:"",
+            visitor_login: "",
+            visitor_logout: "",
             visit_date,
-            visit_status:"pending",
-            visit_completed:"no",
+            visit_status: "pending",
+            visit_completed: "no",
             visit_date,
             ptm_name,
             ptm_mobile,
@@ -43,23 +43,63 @@ var createVisitorController = async (req, res) => {
 
 var visitorListController = async (req, res) => {
     const { visit_status } = req.query;
+    // console.log("Query", visit_status);
     try {
-        var visitors = null ;
-        visitors = visit_status?await VisitorModel.find():await VisitorModel.find(visit_status);
-        if(visitors.length > 0) {
+        var visitors = null;
+        switch (visit_status) {
+            case "pending":
+                visitors = await VisitorModel.find({ visit_status });
+                break;
+            case "accepted":
+                visitors = await VisitorModel.find({ visit_status });
+                break;
+            case "rejected":
+                visitors = await VisitorModel.find({ visit_status });
+                break;
+            default:
+                visitors = await VisitorModel.find();
+                break;
+        }
+        if (visitors.length > 0) {
             return res.json({
-                data : visitors,
+                data: visitors,
                 status: 200
             });
-        }else{
+        } else {
             return res.json({
                 message: "No data found",
                 status: 200
             });
         }
-    }catch (err) {
-        return res.json({ message: err.message, status: err.status});
+    } catch (err) {
+        return res.json({ message: err.message, status: err.status });
     }
 }
 
-export { createVisitorController, visitorListController };
+var visitorApprovalController = async (req, res) => {
+    const { visitor_mobile, approval } = req.query;
+    try {
+        const visitor = await VisitorModel.findOne({ visitor_mobile })
+        if (visitor) {
+            visitor.visit_status = approval;
+            await visitor.save();
+            let message = approval == 'accepted' ? 'Visit Accected' : 'Visit Rejected';
+            return res.json({
+                message: message,
+                status: 200
+            });
+        } else {
+            return res.json({
+                message: "No data found",
+                status: 404
+            });
+        }
+
+    }
+    catch (err) {
+        return res.json({ message: err.message, status: err.status });
+    }
+
+}
+
+export { createVisitorController, visitorListController, visitorApprovalController };
--- backend/src/routes/visitor.js
+++ backend/src/routes/visitor.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { createVisitorController, visitorListController } from '../controllers/VisitorController.js';
+import { createVisitorController, visitorListController, visitorApprovalController } from '../controllers/VisitorController.js';
 import jwt_auth from '../middleware/jwt_auth.js';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 //Routes for creating visit
 router.post('/create-visit', createVisitorController);
 router.get('/visit-list', jwt_auth , visitorListController);
+router.post('/visit-approval', jwt_auth , visitorApprovalController);
 
 export {router as visitorRouter};
